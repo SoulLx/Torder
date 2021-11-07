2cee440273b04f503a32cb35e0e2e7f6cbd51ba9
SoulLx <hjoaovitorsoul@gmail.com>
2021-11-07 15:19:45 -0300

    PUT , GET, POST , DELETE DONE

--- src/teste/index.tsx
+++ src/teste/index.tsx
@@ -1,5 +1,5 @@
 
-import React, { useEffect, useState } from 'react';
+/*import React, { useEffect, useState } from 'react';
 import { ActivityIndicator, FlatList, Text, View } from 'react-native';
 
 export function teste () {
@@ -36,4 +36,35 @@ export function teste () {
     </View>
   );
 };
-      
+ **/
+/*import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList, Text, View } from 'react-native';
+
+export function teste () {
+//const myDataObject = { nome: `joao vitor`,cpf:'123.444.433-01',email:'joaoa@gmail.com', telefone:'(21)994411344'};
+
+const postData = async ( ) =>{
+   const response = await fetch('http://192.168.0.39:3000/api/cliente/6188123b7e03eec5c74019ac', {
+       method: 'DELETE', 
+       headers: {
+         'Content-Type': 'application/json'
+       },
+       body: JSON.stringify(null)
+   });
+
+  const data = await response.json( );
+  console.log(data)
+  // now do whatever you want with the data  
+  
+
+  };
+  postData()
+   return (<View>
+     <Text>aaa</Text>
+   </View>
+   );
+  
+};
+**/
+
+
