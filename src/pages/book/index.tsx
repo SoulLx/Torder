49517e2f7d8ad38bc58d3ee8b448d28260531c0f
Victor <victorhugocoelho2000@gmail.com>
2021-11-25 11:52:16 -0300

    Categoria Post

--- src/pages/book/index.tsx
+++ src/pages/book/index.tsx
@@ -4,20 +4,27 @@ import { TextInput } from 'react-native-gesture-handler';
 import { ArrowLeft } from 'react-native-feather';
 import Modal from "react-native-modal";
 import styles from './styles';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 export function Book ({navigation}:{navigation:any}) {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
   const [visibleCategory, setVisibleCategory] = useState(false);
+  const [visibleSuccess, setVisibleSuccess] = useState(false);
+  const [valueCategory, setValueCategory] = useState({
+    nome: "",
+  })
 
   const getItem = async () => {
      try {
-      const response = await fetch('https://torder-api.vercel.app/api/produto', {
-        method: 'GET', 
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxOWNmOThhNzcyOWQ1NDFmNmNlM2I4MSIsImlhdCI6MTYzNzcwNTE4MCwiZXhwIjoxNjM3NzkxNTgwfQ.fmXV1A0D71O-SrSjFYDde9rGgkB70JZm0ZGxR_X0P3A'
-        },
+       const token = AsyncStorage.getItem('token')
+        const response = await fetch('https://torder-api.vercel.app/api/produto', {
+          method: 'GET', 
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+          },
     })
       const json = await response.json();
       setData(json.produtos);
@@ -32,8 +39,53 @@ export function Book ({navigation}:{navigation:any}) {
     getItem();
   }, []);
 
+
+  const postCategory = async () => {
+    try {
+      const token = await AsyncStorage.getItem('token');
+      const idRestaurante = await AsyncStorage.getItem("restauranteId");
+      
+
+      const item = {
+        nome: valueCategory.nome,
+        restaurante: idRestaurante,
+      }
+
+      const response = await fetch("https://torder-api.vercel.app/api/categoria", {
+        method: 'POST',
+        headers: {
+          'Content-Type':'application/json',
+          'Authorization': 'Bearer '+ token
+        },
+        body: JSON.stringify(item)
+      });
+      const json = await response.json();
+
+      
+    } catch (error) {
+      console.log("error" + error)
+    }
+  }
+  
+
   return (
     <SafeAreaView style={styles.container}>
+
+    <Modal
+      animationIn="slideInUp"
+      animationOutTiming={1000}
+      animationInTiming={600}
+      backdropTransitionOutTiming={800}
+      animationOut="slideOutDown"
+      isVisible={visibleSuccess}
+    >
+      <View style={{backgroundColor:'white'}}>
+        <Text>Categoria Criada</Text>
+        <TouchableOpacity onPress={() => setVisibleSuccess(false)}>
+          <Text>OK</Text>
+        </TouchableOpacity>
+      </View>
+    </Modal>
 
       <Modal           
         animationIn="slideInUp"
@@ -55,9 +107,11 @@ export function Book ({navigation}:{navigation:any}) {
           <TextInput 
             style={{padding: 7, borderWidth: 1, borderRadius: 10, borderColor: "#bdbdbd"}}
             placeholder="Nome da categoria"
+            onChangeText={(text) => setValueCategory({...valueCategory, nome: text})}
+            value={valueCategory.nome}
             
           />
-          <TouchableOpacity style={styles.buttonCategory}>
+          <TouchableOpacity style={styles.buttonCategory} onPress={() => postCategory()}>
             <Text style={{color: 'white'}}>Criar</Text>
           </TouchableOpacity>
         </View>
